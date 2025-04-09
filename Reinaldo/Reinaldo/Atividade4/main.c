37c2972bf4d7d77890ec902d60edc6c7427b4a71
Reinaldo Kamke Camilo <65040710+Snowwi@users.noreply.github.com>
2025-04-08 21:31:26 -0300

    Add files via upload

---
+++ Reinaldo/Reinaldo/Atividade4/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    char opcao;
+    do{
+      system("cls");
+      prinf("Escolha o exercicio da lista 1:\n")
+      prinf("1. Exercicio 1\n")
+      prinf("2. Exercicio 2\n")
+      prinf("3. Exercicio 3\n")
+      prinf("Pressione ESC para sair.\n")
+
+      opcao: getch();
+      system("cls");
+
+
+
+    } while (opcao != 27);
+}
